082bf3281eb1fe1ddbde8a8d8ba4deaf055fd340
ji.moses.ka@gmail.com <ji.moses.ka@gmail.com>
2023-05-04 16:17:29 +0200

    hi

--- script.js
+++ script.js
@@ -8,4 +8,24 @@ eyeColor:"blue",
    console.log('hello there ' + x)
  }
 };
+let secPerson = {
+  firsName : 'noor',
+  lastName : 'sam',
+  age : 28,
+  
+  
+}
+secPerson.age = 55
 console.log(person.sayHello(person.firstName))
+const objSt = person.age.toString()
+console.log(objSt)
+
+let newObj = {
+  secPerson , job : 'designer',
+}
+console.log(newObj)
+delete secPerson.firsName
+
+console.log(secPerson)
+secPerson.every()
+console.log(secPerson)
