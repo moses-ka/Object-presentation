45d7e9cc1a7a06822d5eed7495e9a379010bfb50
moses-ka <ji.moses.ka@gmail.com>
2023-05-06 00:09:15 +0200

    finished an app for teachers

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-let person = {firstName:"John",
-lastName:"Doe",
-age:24,
-eyeColor:"blue",
- sayHello : function Hello(x){
-     
-   
-   console.log('hello there ' + x)
- }
+let school = {
+  students: {
+    "noor ahmadi": { scores: [68, 40] },
+    "sara jamal": { scores: [30, 60] },
+    "mohammed sameer": { scores: [80, 90] },
+    "ahmed saman": { scores: [90, 87] },
+    "ahmed jamal": { scores: [67, 55] },
+    "namiq sam": { scores: [78, 61] },
+    "feras ali": { scores: [71, 63] },
+    "laila khan": { scores: [85, 92] },
+    "ali reza": { scores: [94, 89] },
+    "fatima abdullah": { scores: [77, 81] },
+  },
+  teacher: "jamana ali",
+  subject: "math",
+
+  addScore: function (studentName, score) {
+    this.students[studentName].scores.push(score);
+  },
+  callAverage: function (studentName) {
+    let studentScores = this.students[studentName].scores;
+    let average = studentScores.reduce((a, b) => a + b, 0) / studentScores.length;
+    average = Math.round(average);
+    return average;
+  },
+   addStudent(name, scores) {
+    school.students[name] = { scores };
+  }
+  
 };
-let secPerson = {
-  firsName : 'noor',
-  lastName : 'sam',
-  age : 28,
-  
-  
-}
-secPerson.age = 55
-console.log(person.sayHello(person.firstName))
-const objSt = person.age.toString()
-console.log(objSt)
 
-let newObj = {
-  secPerson , job : 'designer',
-}
-console.log(newObj)
-delete secPerson.firsName
 
-console.log(secPerson)
-secPerson.type ='alpha'
-console.log(secPerson)
+console.log(school.students['feras ali'].scores);
+school.addScore('feras ali', 80);
+console.log(school.students['feras ali'].scores);
+
+console.log(school.callAverage('feras ali'));
+
+school.addStudent('ali mousa', [89, 95,77]);
+
+console.log(school.callAverage('ali mousa'));
